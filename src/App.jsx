30f3e67080a10b65592991c2b92fee9503b8a2a0
rasheed091 <harbdulsalamrasheed33@gmail.com>
2025-10-27 15:46:32 +0100

    redo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import LandingPage from './page/LandingPage'
 import Single from './page/Single'
